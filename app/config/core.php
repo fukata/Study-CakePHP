255d14c499470617ad1f2a90269b8cb547cd8231
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-09-05 11:39:52 +0900

    change security salt and seed.

--- app/config/core.php
+++ app/config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'fbc5ddef0801c61baa77a6f73f6d7875049819da');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '174862551995671078821717636344821627776320940');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
